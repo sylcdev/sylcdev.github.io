889d06bebc0a06ead1de8948600a8801f7252c7b
C1rdec <cedric_lampron@hotmail.com>
2022-03-04 21:03:33 -0500

    Fix return statement

--- src/components/custom-avatar/GithubAvatar.tsx
+++ src/components/custom-avatar/GithubAvatar.tsx
@@ -11,16 +11,17 @@ function GithubAvatar(props: IGithubAvatarProps){
         window.open(`https://github.com/${props.username}`);
     };
 
-    return (<div style={{position:"relative", cursor:"pointer"}}>
-                <Avatar src={`https://avatars.githubusercontent.com/${props.username}`} size={"250"} round className='Custom-Avatar' onClick={open}/>
-                <div style={{position:"absolute", bottom:12, right:22}}>
-                    <Avatar color="white" size={"60"} round className='Custom-Avatar'/>
-                </div>
-                <div style={{position:"absolute", bottom:10, right:20}}>
-                    <Avatar color="white" src={require("../../assets/GithubMark.png")} size={"64"} round className='Custom-Avatar'/>
-                </div>
+    return (
+        <div style={{position:"relative", cursor:"pointer"}}>
+            <Avatar src={`https://avatars.githubusercontent.com/${props.username}`} size={"250"} round className='Custom-Avatar' onClick={open}/>
+            <div style={{position:"absolute", bottom:12, right:22}}>
+                <Avatar color="white" size={"60"} round className='Custom-Avatar'/>
             </div>
-            );
+            <div style={{position:"absolute", bottom:10, right:20}}>
+                <Avatar color="white" src={require("../../assets/GithubMark.png")} size={"64"} round className='Custom-Avatar'/>
+            </div>
+        </div>
+    );
 }
 
 export default GithubAvatar;
