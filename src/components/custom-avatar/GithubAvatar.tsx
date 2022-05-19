c045cb977a3819e32791426447bacdb7c6188554
C1rdec <cedric_lampron@hotmail.com>
2022-05-19 13:30:54 -0400

    Change Octocat size

--- src/components/custom-avatar/GithubAvatar.tsx
+++ src/components/custom-avatar/GithubAvatar.tsx
@@ -5,6 +5,8 @@ interface IGithubAvatarProps{
     username: string,
 }
 
+const octoCatSize =  45;
+
 function GithubAvatar(props: IGithubAvatarProps){
 
     const open = () => {
@@ -15,10 +17,10 @@ function GithubAvatar(props: IGithubAvatarProps){
         <div style={{position:"relative", cursor:"pointer"}}>
             <Avatar src={`https://avatars.githubusercontent.com/${props.username}`} size={"250"} round className='Custom-Avatar' onClick={open}/>
             <div style={{position:"absolute", bottom:12, right:22}}>
-                <Avatar color="white" size={"60"} round className='Custom-Avatar'/>
+                <Avatar color="white" size={octoCatSize.toString()} round className='Custom-Avatar'/>
             </div>
             <div style={{position:"absolute", bottom:10, right:20}}>
-                <Avatar color="white" src={require("../../assets/GithubMark.png")} size={"64"} round className='Custom-Avatar'/>
+                <Avatar color="white" src={require("../../assets/GithubMark.png")} size={(octoCatSize + 4).toString()} round className='Custom-Avatar'/>
             </div>
         </div>
     );
